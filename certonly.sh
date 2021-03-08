accdb9f43ea89d74f4613adefee682dbf971b2b7
Park9eon <quiet9.noise@gmail.com>
2021-03-09 02:05:32 +0900

    yourls 삭제 nginx 삭제

--- certonly.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm -p 80:80 -p 443:443 --network host \
-    -v $PWD/certbot/data/conf:/etc/letsencrypt \
-    -v $PWD/certbot/data/www:/var/www/certbot \
-    -it certbot/certbot certonly \
-    --standalone --non-interactive --agree-tos --email "quiet9.noise@gmail.com" $@
-
